0d6dd92e7e947df46d9be70c222c333044e4fea0
Щемелев Евгений Владимирович <Shchemelev.E.V@sberbank.ru>
2019-06-21 17:32:35 +0300

    2

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const {
   Application,
   loader,
   loader: { resources },
-  Sprite
+  Sprite,
+  Container
 } = PIXI;
 
 const SCREEN_WIDTH = 1200;
 const SCREEN_HEIGHT = 800;
+const REELS_AMOUNT = 5;
+const VISIBLE_SYMBOLS = 4;
 
 // const REEL_WIDTH = SCREEN_WIDTH / 5;
 // const SYMBOL_HEIGHT = SCREEN_HEIGHT / 3;
@@ -81,43 +84,56 @@ function setup() {
     resources['assets\\img\\symbols\\13.png'].texture
   ];
 
-  // Build the reels
-  const reels = [];
-  const reelContainer = new PIXI.Container();
-  for (let i = 0; i < 5; i++) {
-    const rc = new PIXI.Container();
-    rc.x = i * REEL_WIDTH;
-    reelContainer.addChild(rc);
+  const reelContainer = new Container();
 
-    const reel = {
-      container: rc,
-      symbols: [],
-      position: 0,
-      previousPosition: 0,
-      blur: new PIXI.filters.BlurFilter()
-    };
-    reel.blur.blurX = 0;
-    reel.blur.blurY = 0;
-    rc.filters = [reel.blur];
+  // Populate reel
+  const test = new Array(VISIBLE_SYMBOLS).keys();
+  console.log("TCL: setup -> test", test)
+  // .forEach((texture, index) => {
+  //   const sprite = new Sprite(texture);
+  //   sprite.y = index * SYMBOL_SIZE;
+  //   reelContainer.addChild(sprite);
+  // });
 
-    // Build the symbols
-    for (let j = 0; j < 4; j++) {
-      const symbol = new PIXI.Sprite(
-        symbolsTextures[Math.floor(Math.random() * symbolsTextures.length)]
-      );
-      // Scale the symbol to fit symbol area.
-      symbol.y = j * SYMBOL_SIZE;
-      symbol.scale.x = symbol.scale.y = Math.min(
-        SYMBOL_SIZE / symbol.width,
-        SYMBOL_SIZE / symbol.height
-      );
-      symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2);
-      reel.symbols.push(symbol);
-      rc.addChild(symbol);
-    }
-    reels.push(reel);
-  }
   app.stage.addChild(reelContainer);
+
+  // Build the reels
+  // const reels = [];
+  // const reelContainer = new PIXI.Container();
+  // for (let i = 0; i < 5; i++) {
+  //   const rc = new PIXI.Container();
+  //   rc.x = i * REEL_WIDTH;
+  //   reelContainer.addChild(rc);
+
+  //   const reel = {
+  //     container: rc,
+  //     symbols: [],
+  //     position: 0,
+  //     previousPosition: 0,
+  //     blur: new PIXI.filters.BlurFilter()
+  //   };
+  //   reel.blur.blurX = 0;
+  //   reel.blur.blurY = 0;
+  //   rc.filters = [reel.blur];
+
+  //   // Build the symbols
+  //   for (let j = 0; j < 4; j++) {
+  //     const symbol = new PIXI.Sprite(
+  //       symbolsTextures[Math.floor(Math.random() * symbolsTextures.length)]
+  //     );
+  //     // Scale the symbol to fit symbol area.
+  //     symbol.y = j * SYMBOL_SIZE;
+  //     symbol.scale.x = symbol.scale.y = Math.min(
+  //       SYMBOL_SIZE / symbol.width,
+  //       SYMBOL_SIZE / symbol.height
+  //     );
+  //     symbol.x = Math.round((SYMBOL_SIZE - symbol.width) / 2);
+  //     reel.symbols.push(symbol);
+  //     rc.addChild(symbol);
+  //   }
+  //   reels.push(reel);
+  // }
+  // app.stage.addChild(reelContainer);
 
   //Start the game loop
   // app.ticker.add(delta => gameLoop(delta));
